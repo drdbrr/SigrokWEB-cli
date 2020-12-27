220b04e4a2b9ed7a9899d6a6207ae00e472d8015
Evgeniy <drdbrr@yandex.ru>
2020-12-27 14:06:32 +0200

    fixed channels menu

--- src/components/SrChannelsMenu.js
+++ src/components/SrChannelsMenu.js
@@ -1,40 +1,69 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { FormDown } from 'grommet-icons';
 import { ScSrDropDownContent } from '../styled/ScSrDropDownContent';
 import { ScSrDropDown } from '../styled/ScSrDropDown';
+import { ScSrButton } from '../styled/ScSrButton';
 import SrDropDownMenu from './SrDropDownMenu';
 
 import { channelsVar } from '../ApolloClient';
 
-const SrChannelsMenuContent=()=>{
-    console.log('Render SrChannelsMenuContent');
-    const {logic, analog } = channelsVar();
-    const logicChannels = logic.map((item, i)=>{
+const SrChannelsMenuEntries = ({group, label}) =>{
+    console.log('Render SrChannelsMenuEntries')
+    const channelGroup = group.map((item, i)=>{
+        const ref = useRef()
         return(
-            <div key={i + item.name} css={`border:1px solid black; display:flex; flex-direction:row; align-items:center; height:30px`} >
-                <input defaultChecked={item.lineRef.current.visible} type="checkbox" name={item.name} value={item.name} onChange={(e)=>{item.rowRef.current.visible = item.lineRef.current.visible = e.target.checked; }} />
+            <div key={i + item.name} css={`flex: 1 0 21%; margin: 5px;`} >
+                <input ref={ref} defaultChecked={item.lineRef.current.visible} type="checkbox" name={item.name} value={item.name} onChange={(e)=>{item.rowRef.current.visible = item.lineRef.current.visible = e.target.checked; }} />
                 <label css={`color:white`}>{item.name}</label>
             </div>
         );
     });
     
-    const analogChannels = analog.map((item, i)=>{
-        return(
-            <div key={i + item.name} css={`border:1px solid black; display:flex; flex-direction:row; align-items:center; height:30px`} >
-                <input defaultChecked={item.lineRef.current.visible} type="checkbox" name={item.name} value={item.name} onChange={(e)=>{item.rowRef.current.visible = item.lineRef.current.visible = e.target.checked; }} />
-                <label css={`color:white`}>{item.name}</label>
+    const enable = ()=>{
+        group.map((item, i)=> channelGroup[i].props.children[0].ref.current.checked = item.rowRef.current.visible = item.lineRef.current.visible = true)
+    }
+    
+    const disable = ()=>{
+        group.map((item, i)=> channelGroup[i].props.children[0].ref.current.checked = item.rowRef.current.visible = item.lineRef.current.visible = false)
+    }
+    
+    return(
+        <div>
+            <div css={`border-bottom: 1px solid grey; padding-top:7px; padding-bottom:3px; `}>
+                <span css={`color:white; padding-left:10px`} >{ label }</span>
             </div>
-        )
-    });
+            <div css={`display: flex; flex-wrap: wrap;`}>
+                { channelGroup}
+            </div>
+            <div css={`padding-top:5px`}>
+                <ScSrButton css={`float:left; padding-right:7px;`} onClick={enable}>
+                    <span>Enable all</span>
+                </ScSrButton>
+                <ScSrButton css={`float:left; padding-right:7px;`} onClick={disable}>
+                    <span>Disable all</span>
+                </ScSrButton>
+            </div>
+        </div>
+    )
+}
+
+
+const SrChannelsMenuContent=()=>{
+    console.log('Render SrChannelsMenuContent');
+    const {logic, analog } = channelsVar();
     
     return(
         <ScSrDropDownContent>
-            <div>
-                { logicChannels}
-            </div>
-            <div>
-                { analogChannels }
-            </div>
+        <div>
+            { logic ? 
+                <SrChannelsMenuEntries group={logic} label={'Logic'}/>
+                : null
+            }
+            { analog ? 
+                <SrChannelsMenuEntries group={analog} label={'Analog'}/>
+                : null
+            }
+        </div>
         </ScSrDropDownContent>
     )
 }
